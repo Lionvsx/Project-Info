ecdb92d6593707794a89557e471b771d99942ed4
alexs <szpakiewicz.alex@gmail.com>
2022-01-18 19:20:33 +0100

    recup donnees

--- Functions.cs
+++ Functions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Project_Info
@@ -8,7 +9,17 @@ namespace Project_Info
     {
         public static Image ReadImage(string path)
         {
-            
+            byte[] myfile = File.ReadAllBytes(path);
+            //myfile est un vecteur composé d'octets représentant les métadonnées et les données de l'image
+           
+            //Métadonnées du fichier
+            Console.WriteLine("\n Header \n");
+            for (int i = 0; i < 14; i++)
+                Console.Write(myfile[i] + " ");
+            //Métadonnées de l'image
+            Console.WriteLine("\n HEADER INFO \n");
+            for (int i = 14; i< 54; i++)
+                Console.Write(myfile[i] + " ");
         }
 
         public static double ConvertToInt(IEnumerable<byte> data)
