a06c48e44a1c45aef9c50685a79b44c9ee72336b
Leonard Roussard <lroussard@gmail.com>
2022-01-18 18:49:39 +0100

    add convert function

--- Functions.cs
+++ Functions.cs
@@ -1,10 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Project_Info
 {
     public static class Functions
     {
-        public static void ReadImage(string path)
+        public static Image ReadImage(string path)
         {
             
+        }
+
+        public static double ConvertToInt(IEnumerable<byte> data)
+        {
+            double result = 0;
+            var enumerable = data.ToList();
+            for (int i = 0; i < enumerable.Count; i++)
+            {
+                result = result * Math.Pow(256, i);
+            }
+            return result;
         }
     }
 }
