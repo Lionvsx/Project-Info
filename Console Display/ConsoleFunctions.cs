57c856436dc844e7686db8833d19e3ebc076a3ea
Lionvsx <lionvsx@outlook.com>
2022-05-01 01:37:26 +0200

    feat: adding console display functions

--- Console Display/ConsoleFunctions.cs
+++ Console Display/ConsoleFunctions.cs
@@ -22,16 +22,39 @@ namespace Project_Info.Console_Display
             }
         }
         
-        public static void DisplayBoolQRCode(bool[,,] qrCode)
+        public static void DisplayBoolQRCodeMatrix(bool[,,] qrCode, int index)
         {
             for (int line = 0; line < qrCode.GetLength(0); line++)
             {
                 for (int col = 0; col < qrCode.GetLength(1); col++)
                 {
-                    Console.Write(qrCode[line, col, 0] ? "██" : "  ");
+                    Console.Write(qrCode[line, col, index] ? "██" : "  ");
                 }
                 Console.WriteLine();
             }
+        }
+        
+        public static void DisplayBoolQRCode(bool[,] qrCode)
+        {
+            for (int line = 0; line < qrCode.GetLength(0); line++)
+            {
+                for (int col = 0; col < qrCode.GetLength(1); col++)
+                {
+                    Console.Write(qrCode[line, col] ? "██" : "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void DisplayIntChain(int[] chain)
+        {
+            for (var index = 0; index < chain.Length; index++)
+            {
+                if (index % 8 == 0 && index != 0) Console.Write(" ");
+                var bit = chain[index];
+                Console.Write(bit);
+            }
+            Console.WriteLine();
         }
 
         public static void DisplayAppHeader()
